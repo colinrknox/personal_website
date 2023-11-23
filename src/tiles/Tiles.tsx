ace47d0d04f7070d0353576a4ac174e384b251ae
Colin Knox <colin.r.knox@gmail.com>
2023-11-23 01:18:28 -0600

    finished site with backend integration

--- src/tiles/Tiles.tsx
+++ src/tiles/Tiles.tsx
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Tiles.css";
+import { useNavigate } from "react-router-dom";
+import Post from "../post/Post";
 
-const blogPosts = [
-  {
-    title: 'Boost your conversion rate',
-    author: 'Michael Foster',
-    date: 'Mar 16, 2020',
-    imageUrl: 'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-  },
-  {
-    title: 'How to use search engine optimization to drive sales',
-    author: 'Lindsay Walton',
-    date: 'Mar 10, 2020',
-    imageUrl: 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-  },
-  {
-    title: 'Improve your customer experience',
-    author: 'Tom Cook',
-    date: 'Feb 12, 2020',
-    imageUrl: 'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-  },
-];
+const author = "Colin Knox";
+const api = "https://api.cknox.dev";
+
+type Post = {
+  title: string;
+  date: Date;
+  imgUrl: string;
+  contents: string;
+};
 
 function Tiles() {
+  const [blogPosts, setBlogPosts] = useState<{ [key: string]: Post }>({});
+
+  useEffect(() => {
+    fetch("https://api.cknox.dev/posts")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setBlogPosts(data);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }, []);
+
+  const navigate = useNavigate();
+
+  function handleClick(event: React.MouseEvent<HTMLDivElement>) {
+    const divElement = event.currentTarget as HTMLElement;
+    const key = divElement.getAttribute('data-key');
+    if (key)
+      navigate("/post", { state: blogPosts[key].contents });
+  }
+
   return (
     <div className="bg-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -35,12 +53,25 @@ function Tiles() {
           </p>
         </div>
         <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {blogPosts.map((post) => (
-            <div key={post.title} className="rounded-lg shadow-md overflow-hidden hover:shadow-xl">
-              <img className="h-48 w-full object-cover" src={post.imageUrl} alt={post.title} />
+          {Object.entries(blogPosts).map(([key, post]: [string, Post]) => (
+            <div
+              key={key}
+              data-key={key}
+              className="rounded-lg shadow-md overflow-hidden hover:shadow-xl"
+              onClick={handleClick}
+            >
+              <img
+                className="h-48 w-full object-cover"
+                src={api + "/img/" + post.imgUrl}
+                alt={post.title}
+              />
               <div className="p-6">
-                <p className="text-sm font-medium text-gray-500">{post.date} · {post.author}</p>
-                <p className="text-xl font-semibold text-gray-900 mt-2">{post.title}</p>
+                <p className="text-sm font-medium text-gray-500">
+                  {post.date.toString()} · {author}
+                </p>
+                <p className="text-xl font-semibold text-gray-900 mt-2">
+                  {post.title}
+                </p>
               </div>
             </div>
           ))}
