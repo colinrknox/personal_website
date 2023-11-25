0ac974d393e41fbac2e82424efcd1726f027da62
Colin Knox <colin.r.knox@gmail.com>
2023-11-24 22:34:37 -0600

    updated post formatting and added logos back

--- src/post/Post.tsx
+++ src/post/Post.tsx
@@ -1,10 +1,17 @@
 import { useLocation } from "react-router-dom";
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 
 const Post = () => {
-    const location = useLocation();
-    const postContents: string = location.state;
-    return <ReactMarkdown className="markdown p-4 grid-cols-4" children={postContents} />;
-}
+  const location = useLocation();
+  const postContents: string = location.state;
+  return (
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <ReactMarkdown
+        className="markdown max-w-2xl mx-auto"
+        children={postContents}
+      />
+    </div>
+  );
+};
 
 export default Post;
