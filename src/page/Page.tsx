dbad0090a10b89f3304aa8859f4b23458e149f8e
Colin Knox <colin.r.knox@gmail.com>
2023-12-11 12:34:25 -0600

    fixed footer

--- src/page/Page.tsx
+++ src/page/Page.tsx
@@ -5,11 +5,13 @@ import Footer from "./Footer";
 
 const Page = () => {
   return (
-    <>
-      <Nav />
-      <Outlet />
+    <div className="flex flex-col h-screen">
+      <div className="flex-grow">
+        <Nav />
+        <Outlet />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
